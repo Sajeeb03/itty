a53696a08aa1e2a3ad4ebc406452567c661ec278
muntasir <sajeebmuntasir0@gmail.com>
2023-03-17 23:58:14 +0600

    mobile navbar

--- src/Pages/Layouts/Layout.js
+++ src/Pages/Layouts/Layout.js
@@ -9,59 +9,64 @@ import ntfs from "../../Assets/img/ntfs.png"
 import art from "../../Assets/img/art.png"
 import raid from "../../Assets/img/raid.png"
 import { FaDiscord, FaTwitter } from "react-icons/fa"
+import { useState } from "react"
 
 
 
 const Layout = () => {
+    const [showBar,setShowBar] = useState(false)
+
     return (
         <>
             <Header />
             <Outlet className="outlet" />
-            {/* 
-            <div className="mobile_nav">
+
+            <div className={`mobile_nav ${showBar?"show_bar":"hide_bar"}`}>
                 <div className="nav_icon btn_icon">
-                    <img src={btn} alt="" className="" />
+                    <img src={btn} alt="" className="" onClick={()=>setShowBar(!showBar)}/>
                 </div>
-                <div className="nav_icons">
-                    <NavLink to="/" className={({ isActive }) => isActive ? "activated mobile_nav_icons" : "mobile_nav_icons"}>
+
+
+                <div className={`nav_icons`}>
+                    <NavLink to="/" className={({ isActive }) => isActive ? "activated mobile_nav_icons" : "mobile_nav_icons_in"}>
                         <img src={vision} alt="" className="nav_img" />
-                        <p className="nav_option m-0">
+                        <p className="m-0">
                             VISION
                         </p>
                     </NavLink>
-                    <NavLink to="/team" className={({ isActive }) => isActive ? "activated mobile_nav_icons" : "mobile_nav_icons"}>
+                    <NavLink to="/team" className={({ isActive }) => isActive ? "activated mobile_nav_icons" : "mobile_nav_icons_in"}>
                         <img src={team} alt="" className="nav_img" />
-                        <p className="nav_option m-0">
+                        <p className="m-0">
                             TEAM
                         </p>
                     </NavLink>
-                    <NavLink to="/thenft" className={({ isActive }) => isActive ? "activated mobile_nav_icons" : "mobile_nav_icons"}>
+                    <NavLink to="/thenft" className={({ isActive }) => isActive ? "activated mobile_nav_icons" : "mobile_nav_icons_in"}>
                         <img src={ntfs} alt="" className="nav_img" />
-                        <p className="nav_option m-0">
+                        <p className="m-0">
                             THE NFTS
                         </p>
                     </NavLink>
-                    <NavLink to="/arttools" className={({ isActive }) => isActive ? "activated mobile_nav_icons" : "mobile_nav_icons"}>
+                    <NavLink to="/arttools" className={({ isActive }) => isActive ? "activated mobile_nav_icons" : "mobile_nav_icons_in"}>
                         <img src={art} alt="" className="nav_img" />
-                        <p className="nav_option m-0">
+                        <p className="m-0">
                             ART TOOLS
                         </p>
                     </NavLink>
-                    <NavLink to="/raidtools" className={({ isActive }) => isActive ? "activated mobile_nav_icons" : "mobile_nav_icons"}>
+                    <NavLink to="/raidtools" className={({ isActive }) => isActive ? "activated mobile_nav_icons" : "mobile_nav_icons_in"}>
                         <img src={raid} alt="" className="nav_img" />
-                        <p className="nav_option m-0">
+                        <p className="m-0">
                             RAID TOOLS
                         </p>
                     </NavLink>
 
-                    <div className="d-flex align-items-center ms-4 gap-2">
+                    <div className="d-flex align-items-center justify-content-center ms-3 gap-2">
                         <FaDiscord className="social_icons_sm" />
                         <p className="nav_option m-0 text-uppercase">
                             discord
                         </p>
                     </div>
 
-                    <div className="d-flex align-items-center ms-4 gap-2">
+                    <div className="d-flex align-items-center justify-content-center ms-3 gap-2">
                         <FaTwitter className="social_icons_sm" />
                         <p className="nav_option m-0 text-uppercase">
                             Twitter
@@ -69,7 +74,7 @@ const Layout = () => {
                     </div>
 
                 </div>
-            </div> */}
+            </div>
         </>
     )
 }
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -24,45 +24,54 @@ const Header = () => {
                 </Col>
                 <Col md={8} className="d-none d-sm-block">
                     <div className="nav_options d-flex align-items-center justify-content-end gap-2">
+                        <Link to="/" className="text-decoration-none">
                         <div className="d-flex align-items-center">
                             <img src={vision} alt="" className="images" />
-                            <Link to="/" className="text-decoration-none"><p className="m-0 nav_option">
+                                <p className="nav_option">
                                 VISION
-                            </p></Link>
+                                </p>
                         </div>
+                        </Link>
 
+                        <Link to="/team" className="text-decoration-none">
                         <div className="d-flex align-items-center">
                             <img src={team} alt="" className="images" />
-                            <Link to="/team" className="text-decoration-none"><p className="m-0 nav_option">
+                                <p className="nav_option">
                                 TEAM
-                            </p> </Link>
+                                </p> 
                         </div>
+                        </Link>
 
-                        <div className="d-flex align-items-center">
-                            <img src={ntfs} alt="" className="images" />
-                            <Link
+                        <Link
                                 to="/thenft"
                                 className="text-decoration-none"
                             >
-                                <p className="m-0 nav_option">
-                                    THE NFTS
-                                </p></Link>
-                        </div>
+                            <div className="d-flex align-items-center">
+                                <img src={ntfs} alt="" className="images" />
 
+                                <p className="nav_option">
+                                    THE NFTS
+                                </p>
+                            </div>
+                        </Link>
+
+                        <Link to="/arttools" className="text-decoration-none">
                         <div className="d-flex align-items-center">
                             <img src={art} alt="" className="images" />
-                            <Link to="/arttools" className="text-decoration-none"><p className="m-0 nav_option">
+                                <p className="nav_option">
                                 ART TOOLS
-                            </p></Link>
+                                </p>
                         </div>
+                        </Link>
 
+                        <Link to="/raidtools" className="text-decoration-none">
                         <div className="d-flex align-items-center">
                             <img src={raid} alt="" className="images" />
-                            <Link to="/raidtools" className="text-decoration-none"><p className="m-0 nav_option">
+                                <p className="nav_option">
                                 RAID TOOLS
-                            </p></Link>
+                                </p>
                         </div>
-
+                        </Link>
                         <div className="d-flex gap-2 align-items-center">
                             <FaTwitter className="social_icons" />
                             <FaDiscord className="social_icons" />
