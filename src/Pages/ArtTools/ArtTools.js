286b816837ab817dc9c5ceb20b244c0d5d33624e
muntasir <sajeebmuntasir0@gmail.com>
2023-03-18 10:39:57 +0600

    art page done

--- src/Pages/ArtTools/ArtTools.js
+++ src/Pages/ArtTools/ArtTools.js
@@ -1,5 +1,8 @@
 import "./ArtTools.css"
 
+import user from "../../Assets/img/user.png"
+import text from "../../Assets/img/text.png"
+
 const ArtTools = () => {
     return (
         <div className="art_tools_section pb-4">
@@ -19,22 +22,64 @@ const ArtTools = () => {
 
                 <div className="banner_cards">
                     <div>
-                        <div className="banner_card">
-                            1
+                        <div className="banner_card d-flex justify-content-center align-items-center">
+                            <div className="white_card">
+                                <div className="line_one mb-2">
+
+                                </div>
+
+                                <div className="line_two">
+
+                                </div>
+
+                                <div className="line_two my-2">
+
+                                </div>
+
+                                <div className="line_two">
+
+                                </div>
+
+                                <div className="line_three my-2"></div>
+                                <div className="line_three"></div>
+                            </div>
                         </div>
                         <p className="banner_headings mt-2">Connect Wallet</p>
                     </div>
-                    <div>
-                        <div className="banner_card">
-                            2
-                        </div>
-                        <p className="banner_headings mt-2">Customize Banner
 
+
+
+                    <div>
+                        <div className="banner_card second_box p-4 d-flex flex-column justify-content-between">
+                            <div className="d-flex justify-content-end">
+                                <div className="second_box_card d-flex justify-content-end align-items-center p-2">
+                                    <img src={user} alt="" className="" />
+                                </div>
+                            </div>
+
+                            <div>
+                                <div className="second_box_card d-flex justify-content-center align-items-center p-2">
+                                    <img src={text} alt="" className="" />
+                                </div>
+                            </div>
+
+                            <div className="d-flex justify-content-end">
+                                <div className="second_box_card">
+                                    {/* <img src={user} alt="" className="" /> */}
+                                </div>
+                            </div>
+                        </div>
+
+                        <p className="banner_headings mt-2">
+                            Customize Banner
                         </p>
                     </div>
                     <div>
-                        <div className="banner_card">
-                            3
+                        <div className="banner_card d-flex justify-content-center align-items-center">
+                            <div className="second_box_card d-flex justify-content-center align-items-center gap-3 p-2">
+                                <img src={text} alt="" />
+                                <img src={user} alt="" />
+                            </div>
                         </div>
                         <p className="banner_headings mt-2">Generate & Export Banner
 
